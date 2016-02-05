6712b216e1606922cbca7895dccab313ee8153c3
Joe Schubert <joseph.schubert@gmail.com>
2016-02-05 12:34:34 -0500

    Misc: Fix BCL Toggle
    This parameter uses Y/N booleans not 1/0 booloean. Make it check for Y for enabled, might as well have it set Y/N as well.

--- app/src/main/java/com/grarak/kerneladiutor/utils/kernel/Misc.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/kernel/Misc.java
@@ -356,11 +356,11 @@ public class Misc implements Constants {
         return false;
     }
     public static void activateBcl(boolean active, Context context) {
-        Control.runCommand(active ? "1" : "0", BCL, Control.CommandType.GENERIC, context);
+        Control.runCommand(active ? "Y" : "N", BCL, Control.CommandType.GENERIC, context);
     }
 
     public static boolean isBclActive() {
-        return Utils.readFile(BCL).equals("1");
+        return Utils.readFile(BCL).equals("Y");
     }
 
     public static boolean hasBcl() {
