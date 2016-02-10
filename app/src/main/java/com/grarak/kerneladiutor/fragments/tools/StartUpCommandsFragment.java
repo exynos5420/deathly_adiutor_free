abb5a8124766764e305cfdd4dd3c480e68de552c
Joe Schubert <joseph.schubert@gmail.com>
2016-02-10 18:24:15 -0500

    Startup Commands: Add fragment

---
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/tools/StartUpCommandsFragment.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015 Willi Ye
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.grarak.kerneladiutor.fragments.tools;
+
+import android.os.Bundle;
+
+import com.grarak.kerneladiutor.R;
+import com.grarak.kerneladiutor.elements.cards.CardViewItem;
+import com.grarak.kerneladiutor.fragments.RecyclerViewFragment;
+import com.grarak.kerneladiutor.utils.kernel.Info;
+
+
+/**
+ * Created by willi on 25.04.15.
+ */
+public class StartUpCommandsFragment extends RecyclerViewFragment {
+
+    @Override
+    public boolean showApplyOnBoot() {
+        return false;
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+        super.init(savedInstanceState);
+
+        CardViewItem.DCardView mStartupCommandsCard = new CardViewItem.DCardView();
+        mStartupCommandsCard.setTitle(getString(R.string.startup_commands));
+        mStartupCommandsCard.setDescription(Info.getstartupcommands(getActivity()));
+       // mStartupCommandsCard.setDescription("Test");
+
+        addView(mStartupCommandsCard);
+
+    }
+}
